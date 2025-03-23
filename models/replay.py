76edd0d71e3a94fc805ba5c41ee5c3c001835796
tvsanthoshkumar <t.v.santhoshkumar@gmail.com>
2025-03-23 17:01:17 +0530

    Increase number of epochs to 150

--- models/replay.py
+++ models/replay.py
@@ -13,14 +13,14 @@ from utils.toolkit import target2onehot, tensor2numpy
 EPSILON = 1e-8
 
 
-init_epoch = 100
+init_epoch = 150
 init_lr = 0.1
 init_milestones = [60, 120, 170]
 init_lr_decay = 0.1
 init_weight_decay = 0.0005
 
 
-epochs = 100
+epochs = 150
 lrate = 0.1
 milestones = [30, 50]
 lrate_decay = 0.1
