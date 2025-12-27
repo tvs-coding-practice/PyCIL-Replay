ab2b3d19a501a2d90a62851b2e72dcdec2fb424e
Santhosh Tangudu <t.v.santhoshkumar@gmail.com>
2025-12-27 18:35:49 +0530

    Toggle exemplar coreset and kcenter construction

--- models/base.py
+++ models/base.py
@@ -68,8 +68,8 @@ class BaseLearner(object):
     def build_rehearsal_memory(self, data_manager, per_class):
         if self._fixed_memory:
             # self._construct_exemplar_entropy(data_manager, per_class)
-            # self._construct_exemplar_coreset(data_manager, per_class)
-            self._construct_exemplar_kcenter(data_manager, per_class)
+            self._construct_exemplar_coreset(data_manager, per_class)
+            # self._construct_exemplar_kcenter(data_manager, per_class)
             # self._construct_exemplar_unified(data_manager, per_class)
             # self._construct_exemplar_random(data_manager, per_class)
 
